799f1bf8093f850f2493abdda9820a9d26b988c8
Guto <mateus_system@hotmail.com>
2025-03-05 16:28:28 -0300

    entrega de projeto

---
+++ src/components/Button/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button `
+    padding: 10px
+    border: 1px solid #FAFAFE;
+    background-color: #a3c6b6;
+    font-size: 40px;
+    color: #FFFFFF;
+    font-weight: 700;
+    flex: 1; 
+    
+    &: hover {
+    opacity: 0.6;
+    }
+`
